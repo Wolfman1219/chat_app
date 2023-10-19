8c9956d1d0d88637410bc53acbb2d7e8728e90aa
wolfman <ajbborov1234@gmail.com>
2023-10-19 14:57:37 +0500

    remove useless imports

--- get_text_with_site.py
+++ get_text_with_site.py
@@ -1,12 +1,7 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
-from bs4 import BeautifulSoup
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.action_chains import ActionChains
 from gensim.summarization import summarize
 
 # Set the path to the Firefox driver executable
